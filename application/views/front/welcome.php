29f93c3d9e2c974953055417bf96b44e76c407d2
KateYan <kunyan223@gmail.com>
2014-11-29 18:11:00 -0500

    front-start
    added front welcome page about login, campus choose and daily recommed

---
+++ application/views/front/welcome.php
@@ -0,0 +1,65 @@
+<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">-->
+<!--<html xmlns="http://www.w3.org/1999/xhtml">-->
+<!--<head>-->
+<!--    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />-->
+<!--    <title>特价午餐菜单</title>-->
+<!--    <link href="css/masterpage.css" rel="stylesheet" type="text/css" />-->
+<!--    <link href="css/dinner.css" rel="stylesheet" type="text/css" />-->
+<!--</head>-->
+<!--<body>-->
+<!--<header id="Header">11月14号午餐菜单</header>-->
+
+<div class="manaRecommends">
+    <h3>店长推荐</h3>
+    <div class="manaRecom_menu">
+        <img src="css/images/1_04img01.jpg" width="100%"/>
+        <ul>
+            <li>红烧鸭子<i class="borderWidth"></i></li>
+            <li>￥78.00</li>
+        </ul>
+    </div>
+</div>
+<div class="tdySales">
+    <h3>今日6.99特价</h3>
+    <div class="tdySal_menu">
+        <div class="tdyS_m_block">
+            <span class="tdyS_m_img">
+                <img src="css/images/1_04img02.jpg" width="100%" />
+                <span class="menu_title">干锅排骨</span>
+            </span>
+
+        </div>
+        <div class="tdyS_m_block">
+            <span class="tdyS_m_img">
+                <img src="css/images/1_04img02.jpg" width="100%" />
+                <span class="menu_title">蜜辣烤翅</span>
+            </span>
+
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+
+<footer id="Footer">
+    <div class="dinnerS_fMain">
+        <div class="dSales_btnTo">
+            <a class="btn_footer moreDinner" href="dinner.html">想多选？点这里</a>
+            <a href="#" class="btn_footer notInUTSC">我不在UTSC</a>
+            <div class="clear"></div>
+        </div>
+        <input type="tel" class="telephone" placeholder="输入手机号" />
+        <a class="btn_submitOrder btn_nowOrder">立刻下单</a>
+    </div>
+</footer>
+<div class="layer">
+    <div class="black_layer"></div>
+    <div class="layer_summary">
+        <p>对不起，已经超过<span class="sorry">0:00了哦，你可以</span></p>
+        <ul>
+            <li><a class="btn_again">A加入会员延长订餐时间</a></li>
+            <li><a class="btn_again">B明天中午12:00后再订餐</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
