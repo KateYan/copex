eb036724ff824f7994b7dd1660894aaf059da118
KateYan <kunyan223@gmail.com>
2014-12-03 18:13:51 -0500

    try database

--- database/copex.sql
+++ database/copex.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2014 at 11:16 PM
+-- Generation Time: Dec 04, 2014 at 12:13 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -309,7 +309,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`uid`),
   KEY `cid` (`cid`,`vipid`),
   KEY `vipid` (`vipid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10017 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10018 ;
 
 --
 -- Dumping data for table `user`
@@ -320,7 +320,8 @@ INSERT INTO `user` (`uid`, `cid`, `vipid`, `uphone`, `uhash`, `ip`, `ordered`, `
 (10002, 10001, 32716, '201-456-4567', '', '', 0, '0000-00-00 00:00:00', '2014-12-03 21:25:48'),
 (10003, 10002, NULL, '453-453-1234', '', '', 0, '0000-00-00 00:00:00', '2014-12-03 21:25:48'),
 (10004, 10001, NULL, '234-234-1234', '', '', 0, '0000-00-00 00:00:00', '2014-12-03 21:25:48'),
-(10016, 10002, NULL, NULL, '12345', '::1', 0, '2014-12-03 16:50:59', '2014-12-03 21:50:59');
+(10016, 10002, NULL, NULL, '12345', '::1', 0, '2014-12-03 16:50:59', '2014-12-03 21:50:59'),
+(10017, 10002, NULL, NULL, '12345', '::1', 0, '2014-12-03 17:22:41', NULL);
 
 -- --------------------------------------------------------
 
