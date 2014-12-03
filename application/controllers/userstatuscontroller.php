3c0a65980d91e3c2067b822d1a0319eefe0184d7
KateYan <kunyan223@gmail.com>
2014-12-03 17:49:57 -0500

    delete logic

--- application/controllers/userstatuscontroller.php
+++ application/controllers/userstatuscontroller.php
@@ -12,40 +12,14 @@ class Userstatuscontroller extends MY_Controller
 {
 
     /* 微信二级菜单主入口控制方法。
-     * 判断cookie内容
-     * 没有cookie uid 跳转加载选校区界面
-     * 有则判断是否另外两个也有
-     * 没有有另外两个则清空cookie转选校
-     * 三个cookie都有判断user合法性
-     * 合法加载对应菜单
-     * 不合法清空cookie去选校
+     * 根据cookie进入不同页面
     */
     public function checkUserStatus(){
         if(isset($_COOKIE['uid'])){
-            //如果有cookie cid和hash{
-            //  调用validateUser方法
-            //  如果返回值为真{判断用户类型，分别进入不同点餐页面}
-            //  如果返回值为假{delete all cookie, 重定向到选校区方法}
-            //}
-            //如果不全有，删除所有cookie,跳转点餐页面
-            if(isset($_COOKIE['cid'])&&isset($_COOKIE['uhash'])){
-                $valid=$this->validateUser($_COOKIE['uid'],$_COOKIE['uhash']);
-                if($valid){
-
-                }
-                else{
-                    setcookie('uid','');
-                    setcookie('cid','');
-                    setcookie('uhash','');
-                    return redirect('userlogincontroller/loadCampus');
-                }
-            }
-            else{
-                setcookie('uid','');
-                setcookie('cid','');
-                setcookie('uhash','');
-                return redirect('userlogincontroller/loadCampus');
-            }
+            /*
+             * 调用vailidatauer方法
+             * 返回
+             */
          }
         return redirect('userlogincontroller/loadCampus');
     }
