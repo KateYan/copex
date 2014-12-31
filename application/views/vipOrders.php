821207537015f88260e79207308d833763d38ab7
KateYan <kunyan223@gmail.com>
2014-12-30 19:36:39 -0500

    display

--- application/views/vipOrders.php
+++ application/views/vipOrders.php
@@ -33,7 +33,7 @@
     if(isset($orders)){
         foreach($orders as $order){
             echo '<div class="menu_block">';
-            echo '<div class="menuD_img" style="width: 50%;height: inherit;">';
+            echo '<div class="menuD_img" style="width: 50%;">';
             echo '<h4 style="margin-bottom: 5px;">'."订单号：".$order['order']->oid.'</h4>';
             echo '<h4 style="margin-bottom: 5px;">'."下单时间： ".$order['order']->odate.'</h4>';
             echo '<h4 style="margin-bottom: 5px;">'."税款：$ ".$order['order']->tax.'</h4>';
