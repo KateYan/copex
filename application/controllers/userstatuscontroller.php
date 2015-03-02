d2746cad8b8e9f9aaf455787b581881b86848ac0
KateYan <kunyan223@gmail.com>
2015-03-02 06:19:26 -0500

    new welcome page 2 + keep showing welcome

--- application/controllers/userstatuscontroller.php
+++ application/controllers/userstatuscontroller.php
@@ -27,7 +27,8 @@ class Userstatuscontroller extends MY_Controller {
 				//legal user
 				//request user model's method login() to set cookies and sessions for user
 				$this->user->login($oldUser);
-				return redirect('marketcontroller/showDailyMenu');
+//				return redirect('marketcontroller/showDailyMenu');
+				return redirect('userstatuscontroller/welcome');
 			} else {
 				//illegal user
 				setcookie('uid', '', time() - 3600,'/');
