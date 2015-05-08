d44a1f7513597ab2e61263331b40b660e3c77f95
KateYan <kunyan223@gmail.com>
2015-05-07 21:50:36 -0400

    google analysis

--- application/views/mapping.php
+++ application/views/mapping.php
@@ -6,14 +6,6 @@
  * Time: 1:15 AM
  */
 ?>
-<?php
-/**
- * Created by PhpStorm.
- * User: kunyan
- * Date: 1/8/2015
- * Time: 10:16 AM
- */
-?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html>
 <head>
@@ -23,10 +15,20 @@
     <meta content="yes" name="apple-mobile-web-app-capable" />
     <meta content="black" name="apple-mobile-web-app-status-bar-style" />
     <meta content="telephone=no" name="format-detection" />
-    <meta name="viewport" content="width=device-width; maximum-scale=1.0;  user-scalable=no; initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0,  user-scalable=no,initial-scale=1.0" />
     <title>等待起航</title>
     <link href="/copex/css/masterpage.css" rel="stylesheet" type="text/css" />
     <link href="/copex/css/bgRed.css" rel="stylesheet" type="text/css" />
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-62513157-2', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 <body>
 <div class="timeout">
