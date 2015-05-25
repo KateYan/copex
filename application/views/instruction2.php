89b55aa743bb8269e53499bf269337e0bf65a685
KateYan <kunyan223@gmail.com>
2015-05-25 07:26:42 -0400

    google analysis 3

--- application/views/instruction2.php
+++ application/views/instruction2.php
@@ -17,9 +17,18 @@
     <meta content="black" name="apple-mobile-web-app-status-bar-style" />
     <meta content="telephone=no" name="format-detection" />
     <meta name="viewport" content="width=device-width; maximum-scale=1.0;  user-scalable=no; initial-scale=1.0" />
-<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href="/copex/css/reset.css" rel="stylesheet" type="text/css" />
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-62513157-2', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 <body>
 <img src="/copex/images/page02_01.jpg" width="100%" />
--- application/views/ordersuccess.php
+++ application/views/ordersuccess.php
@@ -21,6 +21,16 @@
     <title>订单成功</title>
     <link href="../../css/masterpage.css" rel="stylesheet" type="text/css" />
     <link href="../../css/bgRed.css" rel="stylesheet" type="text/css" />
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-62513157-2', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
 <body>
 <div class="orderGene">
